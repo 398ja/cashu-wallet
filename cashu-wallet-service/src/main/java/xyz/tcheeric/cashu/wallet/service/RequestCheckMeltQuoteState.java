67339e3d7d32352a3999908451e6a1c88b086cc3
erict875 <erict875@tuta.io>
2025-08-02 22:27:38 +0100

    Add JUnit and Mockito dependencies for testing; fix response type in RequestCheckMeltQuoteState

--- cashu-wallet-service/src/main/java/xyz/tcheeric/cashu/wallet/service/RequestCheckMeltQuoteState.java
+++ cashu-wallet-service/src/main/java/xyz/tcheeric/cashu/wallet/service/RequestCheckMeltQuoteState.java
@@ -2,12 +2,13 @@ package xyz.tcheeric.cashu.wallet.service;
 
 import lombok.NonNull;
 import xyz.tcheeric.cashu.entities.annotation.Nut;
+import xyz.tcheeric.cashu.entities.rest.PostMeltQuoteResponse;
 import xyz.tcheeric.cashu.entities.rest.PostMintQuoteResponse;
 
 @Nut(5)
-public class RequestCheckMeltQuoteState extends AbstractRequestBase<PostMintQuoteResponse, Void> {
+public class RequestCheckMeltQuoteState extends AbstractRequestBase<PostMeltQuoteResponse, Void> {
 
     public RequestCheckMeltQuoteState(@NonNull String baseUrl, @NonNull String quoteId, @NonNull String paymentMethod) {
-        super(baseUrl, "/melt/quote/" + paymentMethod + "/" + quoteId, PostMintQuoteResponse.class);
+        super(baseUrl, "/melt/quote/" + paymentMethod + "/" + quoteId, PostMeltQuoteResponse.class);
     }
 }
