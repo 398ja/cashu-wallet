f637e754942fbb15108c81ed7ef889fcb4671078
erict875 <erict875@tuta.io>
2025-08-22 22:56:35 +0100

    Refactor: migrate wallet service classes to wallet client package

--- cashu-wallet-client/src/main/java/xyz/tcheeric/cashu/wallet/client/impl/RequestKeySetPublicKey.java
+++ cashu-wallet-client/src/main/java/xyz/tcheeric/cashu/wallet/client/impl/RequestKeySetPublicKey.java
@@ -1,8 +1,9 @@
-package xyz.tcheeric.cashu.wallet.service;
+package xyz.tcheeric.cashu.wallet.client.impl;
 
 import lombok.NonNull;
 import xyz.tcheeric.cashu.entities.annotation.Nut;
 import xyz.tcheeric.cashu.entities.rest.GetKeySetsResponse;
+import xyz.tcheeric.cashu.wallet.client.AbstractRequestBase;
 
 @Nut(1)
 public class RequestKeySetPublicKey extends AbstractRequestBase<GetKeySetsResponse, Void> {
